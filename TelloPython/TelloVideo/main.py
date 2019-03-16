1b05558754cca5535a0ad0daba6b4fac46f7527b
Ari <arimoralesjordan@gmail.com>
2019-03-16 01:56:36 -0500

    mmm

---
+++ TelloPython/TelloVideo/main.py
@@ -0,0 +1,15 @@
+import tello
+from tello_control_ui import TelloUI
+
+
+def main():
+
+    drone = tello.Tello('10.0.1.14', 8889, False, .3, '10.0.1.24')
+    vplayer = TelloUI(drone, "./img")
+
+    # start the Tkinter mainloop
+    vplayer.root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
